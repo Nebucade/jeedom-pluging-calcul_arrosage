f7db21c3bbf1a0fa0b897dec11ac0e59564f1617
Nicolas Lurvois <nicolas.lurvois@netwyse.fr>
2018-07-24 12:07:13 +0200

    Creation des commandes

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -22,6 +22,28 @@ if (!isConnect()) {
     die();
 }
 ?>
+<div class="col-sm-6">
+    <legend>{{Météo}}</legend>
+    <form class="form-horizontal">
+			<fieldset>
+				<div class="form-group">
+					<label class="col-lg-5 control-label">{{Météo actuel}}</label>
+					<div class="col-lg-6">
+						<div class="input-group">
+							<input class="configKey form-control input-sm" data-l1key="cmdConditionActuel"/>
+							<span class="input-group-btn">
+								<a class="btn btn-success btn-sm listAction">
+									<i class="fa fa-list-alt"></i>
+								</a>
+							</span>
+						</div>
+						<input type="text" class="configKey"  data-l1key="ponditionActuel" />
+					</div>
+                </div>
+            </fieldset>
+    </form>
+</div>
+
 <form class="form-horizontal">
     <fieldset>
         <div class="form-group">
