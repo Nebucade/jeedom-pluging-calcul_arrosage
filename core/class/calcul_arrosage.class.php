7a8f746b4e832ff35f7c68532adeed5463e1c058
Nicolas Lurvois <nicolas.lurvois@netwyse.fr>
2018-07-24 13:53:34 +0200

    Creation des commandes

--- core/class/calcul_arrosage.class.php
+++ core/class/calcul_arrosage.class.php
@@ -156,6 +156,7 @@ class calcul_arrosage extends eqLogic {
         
         if (config::byKey("cmdConditionActuel","calcul_arrosage") != "")
         {
+            $eqlogic = $this->getEqLogic();
             log::add("calcul_arrosage","info","Value of ID Condition Param :".jeedom::evaluateExpression(config::byKey("cmdConditionActuel","calcul_arrosage")));
             log::add("calcul_arrosage","info","Last Value of Condition :".$eqlogic->byLogicalId('PluieJournee'));
             
