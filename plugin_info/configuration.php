946d14a574c9537c39c338f8df8826614838ec88
Nicolas Lurvois <nicolas.lurvois@netwyse.fr>
2018-07-24 09:57:24 +0200

    Creation des commandes

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -25,9 +25,9 @@ if (!isConnect()) {
 <form class="form-horizontal">
     <fieldset>
         <div class="form-group">
-            <label class="col-lg-4 control-label">{{Global param 1}}</label>
+            <label class="col-lg-4 control-label">ID condition</label>
             <div class="col-lg-2">
-                <input class="configKey form-control" data-l1key="param1" />
+                <input class="configKey form-control" data-l1key="paramIdCondition" />
             </div>
         </div>
         <div class="form-group">
