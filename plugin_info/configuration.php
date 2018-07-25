2f0277f8af86cb8918b434bb3f6c76f1ed5509fb
Nicolas Lurvois <nicolas.lurvois@netwyse.fr>
2018-07-25 14:57:38 +0200

    Ajout de la temperature max de la journée

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -115,3 +115,77 @@
 		</form>
 	</div>
 </div>
+
+<script>
+	$("body").on('click', ".listAction", function() {
+		var el = $(this).closest('.input-group').find('input');
+		jeedom.cmd.getSelectModal({}, function (result) {
+			el.value(result.human);
+		});
+	});
+	$.ajax({
+		type: "POST",
+		timeout:8000,
+		url: "core/ajax/config.ajax.php",
+		data: {
+			action:'getKey',
+			key:'{"configuration":""}',
+			plugin:'arrosageAuto',
+		},
+		dataType: 'json',
+		error: function(request, status, error) {
+			handleAjaxError(request, status, error);
+		},
+		success: function(data) {
+			if (data.state != 'ok') {
+				$('#div_alert').showAlert({message: data.result, level: 'danger'});
+				return;
+			}
+			if (data.result['configuration']!=''){
+				var TypePlantation= new Object();
+				$.each(data.result['configuration'], function(param,valeur){
+					switch(typeof(valeur)){
+						case 'object':
+							$.each(valeur, function(TypePlantationkey,value ){
+								if (typeof(TypePlantation[TypePlantationkey]) === 'undefined')
+									TypePlantation[TypePlantationkey]= new Object();
+								if (typeof(TypePlantation[TypePlantationkey]['configuration']) === 'undefined')
+									TypePlantation[TypePlantationkey]['configuration']= new Object();
+								TypePlantation[TypePlantationkey]['configuration'][param]=value;
+							});
+						break;
+						case 'string':
+							if (typeof(TypePlantation[0]) === 'undefined')
+								TypePlantation[0]= new Object();
+							if (typeof(TypePlantation[0]['configuration']) === 'undefined')
+								TypePlantation[0]['configuration']= new Object();
+							TypePlantation[0]['configuration'][param]=valeur;
+						break;
+					}
+				});
+				$.each(TypePlantation, function(id,data){
+					AddTypePlantation($('#table_type_plantation tbody'),data);
+				});
+			}
+		}
+	});
+	$('#bt_AddTypePlantation').on('click',function(){
+		AddTypePlantation($('#table_type_plantation tbody'),'');
+	});
+	$('body').on('click','#bt_RemoveTypePlantation',function(){
+		$(this).closest('tr').remove();
+	});
+	function AddTypePlantation(_el,data){
+		var tr=$('<tr>')
+			.append($('<td>')
+				.append($('<div class="input-group">')
+					.append($('<span class="input-group-btn">')
+						.append($('<a class="btn btn-default btn-sm bt_RemoveTypePlantation">')
+							.append($('<i class="fa fa-minus-circle">'))))
+					.append($('<input class="configKey form-control input-sm "data-l1key="configuration" data-l2key="type">'))))
+			.append($('<td>')
+				.append($('<input class="configKey form-control input-sm" data-l1key="configuration" data-l2key="volume">')));
+		_el.append(tr);
+		_el.find('tr:last').setValues(data, '.configKey');
+	}
+</script>
